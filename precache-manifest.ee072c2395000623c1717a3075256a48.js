df09d8ecbd996c68d560df1164637599bcd5b488
jabbiati <jabbiati@yahoo.com>
2020-05-12 13:31:21 -0400

    Updates

---
+++ precache-manifest.ee072c2395000623c1717a3075256a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd81a48b5313cc56b126b7f89086924c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "6c6fbf2843e4dfdf6f29",
+    "url": "/monsters/static/css/main.a1f49960.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6fbf2843e4dfdf6f29",
+    "url": "/monsters/static/js/main.5654562b.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
